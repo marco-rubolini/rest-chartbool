2b4e8bb391c8cea78228ffeb61cf2fd6f24fc69d
Marco <rubolini.marco@gmail.com>
2020-06-10 23:22:07 +0200

    Grafici realizzati
    Problema ordine crescente mesi

--- main.js
+++ main.js
@@ -45,6 +45,8 @@ $.ajax({
         // Oggetto che conterrà il totale di vendite raggruppate per mese
 
         var vendite_mensili = {};
+        var contributo_venditore = {}
+        var fatturato_totale = 0;
 
         for (var i = 0; i < data.length; i++) {
             // Recupero l'elemento corrente che corrisponde ad un oggetto
@@ -53,23 +55,58 @@ $.ajax({
             console.log(vendite_azienda);
             // Recupero la data ddi ogni singola vendita
             var data_vendita = vendite_azienda.date;
-            console.log(data_vendita);
-            // Estraggo il mese dalla data di vendita
+            console.log('data di vendita: ' + data_vendita);
+            // Estraggo il numero del mese dalla data di vendita
             // var mese = moment(data_vendita); errore
-            var mese = moment(data_vendita, "DD/MM/YYYY").format('MMMM')
-            console.log(mese);
+            var mese_numerico = moment(data_vendita, "DD/MM/YYYY").format('MM');
+            console.log('Numero del mese corrispondente:' + mese_numerico);
+            // Estraggo il nome del mese dalla data di vendita
+            var mese_alfanumerico = moment(data_vendita, "DD/MM/YYYY").format('MMMM')
+            console.log('Mese della vendita: ' + mese_alfanumerico);
             // Recupero l'importo di ogni singola vendita
             var importo_vendita = vendite_azienda.amount;
-            console.log(importo_vendita);
+            console.log('Importo vendita: ' + importo_vendita);
+            // Recupero il nome del venditore
+            var venditore = vendite_azienda.salesman;
+            console.log('Venditore: ' + venditore);
+            // Calcolo il fatturato totale dato dalla somma di ogni vendita
+            fatturato_totale += importo_vendita
+            console.log('Fatturato Totale: ' + fatturato_totale);
 
-            if (!vendite_mensili.hasOwnProperty(mese)) {
-                vendite_mensili[mese] = importo_vendita
+            // Verifico se l'oggetto vendite mensili contiene già la chiave del mese
+            if (!vendite_mensili.hasOwnProperty(mese_alfanumerico)) {
+                // il mese corrente non è presente nell'oggetto dei mesi
+                // creo una nuova chiave con il mese corrente
+                // e assegno il valore con l'importo della vendita corrente
+                vendite_mensili[mese_alfanumerico] = importo_vendita
             } else {
-                vendite_mensili[mese] += importo_vendita
+                // la chiave corrispondente a questo mese esiste già!
+                // il mese corrente è già stato incontrato in qualche iterazione precedente
+                // incremento il totale salvato per il mese corrente con l'importo della vendita corrente
+                vendite_mensili[mese_alfanumerico] += importo_vendita
             }
-            console.log(vendite_mensili);
 
+
+            // Verifico se l'oggetto contributo venditore contiene già la chiave del venditore
+            if (!contributo_venditore.hasOwnProperty(venditore)) {
+                // il venditore corrente non è presente nell'oggetto contributo venditori
+                // creo una nuova chiave con il venditore corrente
+                // e assegno il valore con l'importo della vendita corrente
+                contributo_venditore[venditore] = importo_vendita;
+                console.log('Contributo del venditore: ' + contributo_venditore[venditore]);
+            } else {
+                // la chiave corrispondente a questo venditore esiste già!
+                // il venditore corrente è già stato incontrato in qualche iterazione precedente
+                // incremento il totale salvato per il venditore corrente con l'importo della vendita corrente
+                contributo_venditore[venditore] += importo_vendita;
+                console.log('Contributo del venditore: ' + contributo_venditore[venditore]);
+            }
+
+
+            // var percentuale = contributo_venditore[venditore] / fatturato_totale * 100;
+            // console.log('percentuale' + percentuale);
         }
+
 
             var chiavi = Object.keys(vendite_mensili);
             console.log(chiavi);
@@ -83,10 +120,10 @@ $.ajax({
                 data: {
                     labels: chiavi,
                     datasets: [{
-                        label: '# of Votes',
+                        label: 'Vendite mensili',
                         data: valori,
                         backgroundColor: [
-                            // 'rgba(255, 99, 132, 0.2)',
+                            'rgba(255, 99, 132, 0.2)',
                             // 'rgba(54, 162, 235, 0.2)',
                             // 'rgba(255, 206, 86, 0.2)',
                             // 'rgba(75, 192, 192, 0.2)',
@@ -94,7 +131,7 @@ $.ajax({
                             // 'rgba(255, 159, 64, 0.2)'
                         ],
                         borderColor: [
-                            // 'rgba(255, 99, 132, 1)',
+                            'rgba(255, 99, 132, 1)',
                             // 'rgba(54, 162, 235, 1)',
                             // 'rgba(255, 206, 86, 1)',
                             // 'rgba(75, 192, 192, 1)',
@@ -114,6 +151,62 @@ $.ajax({
                     }
                 }
             });
+
+            var chiavi = Object.keys(contributo_venditore);
+            console.log(chiavi);
+
+            var valori = Object.values(contributo_venditore);
+            console.log(valori);
+            var contributo = [];
+            for (var i = 0; i < valori.length; i++) {
+                var percentuale = (valori[i]/fatturato_totale*100).toFixed(2);
+                // percentuale.toFixed(2);
+                console.log(percentuale);
+                contributo.push(percentuale)
+            }
+            console.log(contributo);
+
+
+
+
+            var ctx = $('#myChart2')[0].getContext('2d');
+            var myChart = new Chart(ctx, {
+                type: 'pie',
+                data: {
+                    labels: chiavi,
+                    datasets: [{
+                        label: 'Vendite mensili',
+                        data: contributo,
+                        backgroundColor: [
+                            'rgba(255, 99, 132, 0.2)',
+                            'rgba(54, 162, 235, 0.2)',
+                            'rgba(255, 206, 86, 0.2)',
+                            'rgba(75, 192, 192, 0.2)',
+                            // 'rgba(153, 102, 255, 0.2)',
+                            // 'rgba(255, 159, 64, 0.2)'
+                        ],
+                        borderColor: [
+                            'rgba(255, 99, 132, 1)',
+                            'rgba(54, 162, 235, 1)',
+                            'rgba(255, 206, 86, 1)',
+                            'rgba(75, 192, 192, 1)',
+                            // 'rgba(153, 102, 255, 1)',
+                            // 'rgba(255, 159, 64, 1)'
+                        ],
+                        borderWidth: 1
+                    }]
+                },
+                options: {
+                    scales: {
+                        yAxes: [{
+                            ticks: {
+                                beginAtZero: true
+                            }
+                        }]
+                    }
+                }
+            });
+
 
 
     },
